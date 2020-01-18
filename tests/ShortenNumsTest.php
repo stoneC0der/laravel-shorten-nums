91372f9d3dadacd0ecf5cc306f896ba2976b8722
Cedric Megnie <blackleeder@gmail.com>
2020-01-18 14:13:25 +0000

    Rename test method, added trillion, renamed variable

--- tests/ShortenNumsTest.php
+++ tests/ShortenNumsTest.php
@@ -5,34 +5,24 @@ namespace Stonec0der\ShortenNums\Tests;
 use PHPUnit\Framework\TestCase;
 use Stonec0der\ShortenNums\ShortenNumsFacade;
 
-class ExampleTest extends TestCase
+class ShortenNumsTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function is_readableNumber()
     {
-    	$random = ShortenNumsFacade::shortenNumber('2000', 4);
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}k$|((?!(0))^[\d]{1,3}\.\d{1,1}k$))/i', $random);
+    	$random = ShortenNumsFacade::readableNumber('2000',3);
+        $this->assertRegExp('/((?!(0))^[\d]{1,3}k$|((?!(0))^[\d]{1,3}\.\d{1,}k$))/i', $random);
 
-    	$one_k = ShortenNumsFacade::shortenThousand('1000');
-        $this->assertRegExp('/((?!(0))^[\d]{1,1}k$|((?!(0))^[\d]{1,1}\.\d{1,1}k$))/i', $one_k);
+        $thousands = ShortenNumsFacade::readableThousand('120300');
+        $this->assertRegExp('/((?!(0))^[\d]{1,3}k$|((?!(0))^[\d]{1,3}\.\d{1,}k$))/i', $thousands);
 
-        $ten_k = ShortenNumsFacade::shortenTenThousand('10000');
-        $this->assertRegExp('/((?!(0))^[\d]{1,2}k$|((?!(0))^[\d]{1,2}\.\d{1,1}k$))/i', $ten_k);
+        $millions = ShortenNumsFacade::readableMillion('359050235');
+        $this->assertRegExp('/((?!(0))^[\d]{1,3}M$|((?!(0))^[\d]{1,3}\.\d{1,}M$))/i', $millions);
 
-        $handred_k = ShortenNumsFacade::shortenHandredThousand('120300');
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}k$|((?!(0))^[\d]{1,3}\.\d{1,1}k$))/i', $handred_k);
+        $billion = ShortenNumsFacade::readableBillion('4077578213');
+        $this->assertRegExp('/((?!(0))^[\d]{1,3}B$|((?!(0))^[\d]{1,3}\.\d{1,}B$))/i', $billion);
 
-        $one_M = ShortenNumsFacade::shortenMillion('1253003');
-        $this->assertRegExp('/((?!(0))^[\d]{1,1}M$|((?!(0))^[\d]{1,1}\.\d{1,1}M$))/i', $one_M);
-
-        $ten_M = ShortenNumsFacade::shortenTenMillion('93952000');
-        $this->assertRegExp('/((?!(0))^[\d]{1,2}M$|((?!(0))^[\d]{1,2}\.\d{1,1}M$))/i', $ten_M);
-        $this->assertEquals('93.9M', $ten_M);
-
-        $handred_M = ShortenNumsFacade::shortenHandredMillion('359050235');
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}M$|((?!(0))^[\d]{1,3}\.\d{1,1}M$))/i', $handred_M);
-
-        $one_B = ShortenNumsFacade::shortenBillion('3500000000');
-        $this->assertRegExp('/((?!(0))^[\d]{1,1}B$|((?!(0))^[\d]{1,1}\.\d{1,1}B$))/i', $one_B);
+        $trillions = ShortenNumsFacade::readableTrillion('407757821309809',4);
+        $this->assertRegExp('/((?!(0))^[\d]{1,3}T$|((?!(0))^[\d]{1,3}\.\d{1,}T$))/i', $trillions);
     }
 }
