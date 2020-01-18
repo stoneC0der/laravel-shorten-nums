ec5232f4a8a838488caa3121df019d059131511f
Cedric Megnie <blackleeder@gmail.com>
2020-01-18 14:09:37 +0000

    Improved method, combined shortenThousand, shortenTenThous to formatThousand and so on

--- src/ShortenNums.php
+++ src/ShortenNums.php
@@ -18,341 +18,161 @@ namespace Stonec0der\ShortenNums;
  */
 class ShortenNums
 {
-    // Build your next great package.
-    public static function shortenNumber($number, $length = null)
+	private $thousand_format;
+	private $million_format;
+	private $billion_format;
+	private $trillion_format;
+	// TODO:  Add method to use $value / 999 for 999-999999 to return 0.9K instead of rounded it to 1K
+
+	/**
+	 * Convert long number to readable Numbers 1000 => 1K
+	 * 
+	 * @param string $value This should be pass as a string for now.
+	 * @param int $precision Number of number after decimal point.
+	 * @return string
+	 */
+	public static function formatNumber($value, $precision): string
 	{
 		/**
     	 * The length of the provided number if length was not set.
     	 * @var int
     	 */
-        $number_length = strlen((string)$number);
+        $number_length = strlen((string)$value);
 
         /**
-	     * @var string $shortened The shortened value.
+	     * @var string $formated_number The formated_number value.
 	     */
-	    $shortened;
+	    $formated_number;
 
-	    self::validateNumber($number);
-	    self::validateLength($length, $number, $number_length);
+	    self::validateNumber($value);
 	    
-	    if (!is_null($length)) 
-	    {
-	    	switch ($length) {
-	    		case 4:
-	    			$shortened = self::shortenThousand($number);
-	    			break;
-	    		case 5:
-	    			$shortened = self::shortenThenThousand($number);
-	    			break;
-	    		case 6:
-	    			$shortened = self::shortenHandredThousand($number);
-	    			break;
-	    		case 7:
-	    			$shortened = self::shortenMillion($number);
-	    			break;
-	    		case 8:
-	    			$shortened = self::shortenTenMillion($number);
-	    			break;
-	    		case 9:
-	    			$shortened = self::shortenHandredMillion($number);
-	    			break;
-	    		case 10:
-	    			$shortened = self::shortenBillion($number);
-	    			break;
-	    	    default:
-	    			$shortened = self::notSupported($number);
-	    			break;
-	    	}
-	    	return $shortened;
-	    } 
-	    else 
-	    {
-	        switch ($number_length) {
-	    		case 4:
-	    			$shortened = self::shortenThousand($number);
-	    			break;
-	    		case 5:
-	    			$shortened = self::shortenThenThousand($number);
-	    			break;
-	    		case 6:
-	    			$shortened = self::shortenHandredThousand($number);
-	    			break;
-	    		case 7:
-	    			$shortened = self::shortenMillion($number);
-	    			break;
-	    		case 8:
-	    			$shortened = self::shortenTenMillion($number);
-	    			break;
-	    		case 9:
-	    			$shortened = self::shortenHandredMillion($number);
-	    			break;
-	    		case 10:
-	    			$shortened = self::shortenBillion($number);
-	    			break;
-	    		default:
-	    			$shortened = self::notSupported($number);
-	    			break;
-	    	}
-	    	return $shortened;
-	    }
+		switch ($number_length) {
+			case 4:
+			case 5:
+			case 6:
+				$formated_number = self::formatThousand($value, $precision);
+				break;
+			case 7:
+			case 8:
+			case 9:
+				$formated_number = self::formatMillion($value, $precision);
+				break;
+			case 10:
+			case 11:
+			case 12:
+				$formated_number = self::formatBillion($value, $precision);
+				break;
+			case 13:
+			case 14:
+			case 15:
+				$formated_number = self::formatTrillion($value, $precision);
+				break;
+			default:
+				$formated_number = self::notSupported($value);
+				break;
+		}
+		return $formated_number;
 	}
 
 	/**
-	 * Convert 1,000 place to 1K
+	 * Convert 1000 => 1K, 10000 => 10K & 1000000 => 100K
+	 * 
 	 * @param  int/string $number
+	 * @param int $precision Number of number after decimal point.
 	 * @return string 	A string number formated as 1K-1.5K
 	 */		
-	public static function shortenThousand($number)
+	public static function formatThousand($value, $precision): string
 	{
-		$length = 4;
+		$format = 1000; // If you dont wnat 0.9k but straight 1k for 999
+		// $treshold2 = 999; // If you want 0.9k for 999
+		$range = [999, 999999];
+		$suffix = 'K';
+		// Check if value is a valid integer and does not start with 0, and force user to pass value as string
+		self::validateNumber($value);
+		self::validateRange($value, $range);
 
-		self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 4), ($length - 3));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 3), ($length));
-        
-        if ($others === "000") {
-            return $leading_number.'K';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 4), ($length - 2));
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            // Remove 0 in 9.0K
-            return (preg_match('/\.0K$/', $arr[0].'.'.$arr[1].'K')) ? $arr[0].'K' : $arr[0].'.'.$arr[1].'K';
-        }
-	}
+		if (isset(self::$thousand_format))
+			$clean_number = $value / self::$thousand_format;
+		$clean_number = $value / $format;
+		// Round and format number
+		$formated_number = number_format($clean_number,$precision);
 
-	/**
-	 * Convert 10,000 place to 10K
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 10K-10.5K
-	 */
-	public static function shortenTenThousand($number)
-	{
-		$length = 5;
-
-		self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 5), ($length - 3));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 3), ($length));
-
-        if ($others === "000") {
-            
-            return $leading_number.'K';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 5), ($length - 2)); 
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            return (preg_match('/\.0K$/', $arr[0].$arr[1].'.'.$arr[2].'K')) ? $arr[0].$arr[1].'K' : $arr[0].$arr[1].'.'.$arr[2].'K';
-        }
-	}
-
-	/**
-	 * Convert 100,000 place to 100K
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 100K-100.5K
-	 */
-	public static function shortenHandredThousand($number)
-	{
-		$length = 6;
-
-		self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 6), ($length - 3));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 3), ($length));
-
-        if ($others === "000") {
-            
-            return $leading_number.'K';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 6), ($length - 2)); 
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            return (preg_match('/\.0K$/', $arr[0].$arr[1].$arr[2].'.'.$arr[3].'K')) ? $arr[0].$arr[1].$arr[2].'K' : $arr[0].$arr[1].$arr[2].'.'.$arr[3].'K';
-        }
+		return (preg_match('/\\d\.0$/', $formated_number)) ? rtrim(rtrim($formated_number,'0'), '.').$suffix : $formated_number.$suffix;
 	}
 
 	/**
 	 * Convert 1,000,000 place to 1M
 	 * @param  int/string $number
+	 * @param int $precision Number of number after decimal point.
 	 * @return string 	A string number formated as 1M-1.5M
 	 */
-	public static function shortenMillion($number)
+	public static function formatMillion($value, $precision): string
 	{
-		$length = 7;
+		$format = 1000000; // If you dont wnat 0.9M but straight 1k for 999.999
+		// $treshold2 = 999; // If you want 0.9M for 999999
+		$range = [999999, 999999999];
+		$suffix = 'M';
+		// Check if value is a valid integer and does not start with 0, and force user to pass value as string
+		self::validateNumber($value);
+		self::validateRange($value, $range);
 
-		self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 7), ($length - 6));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 6), ($length));
-        if ($others === "000000") {
-            
-            return $leading_number.'M';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 7), ($length - 3)); 
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            return (preg_match('/\.0M$/', $arr[0].'.'.$arr[1].'M')) ? $arr[0].'M' : $arr[0].'.'.$arr[1].'M';
-        }
-	}
+		if (isset(self::$million_format))
+			$clean_number = $value / self::$million_format;
+		$clean_number = $value / $format;
+		// Round and format number
+		$formated_number = number_format($clean_number,$precision);
 
-	/**
-	 * Convert 10,000,000 place to 10M
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 10M-10.5M
-	 */
-	public static function shortenTenMillion($number)
-	{
-	    $length = 8;
-
-	    self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 8), ($length - 6));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 6), ($length));
-        if ($others === "000000") {
-            
-            return $leading_number.'M';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 8), ($length - 5)); 
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            return (preg_match('/\.0M$/', $arr[0].$arr[1].'.'.$arr[2].'M')) ? $arr[0].$arr[1].'M' : $arr[0].$arr[1].'.'.$arr[2].'M';
-        }
-	}
-
-	/**
-	 * Convert 100,000,000 place to 100M
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 100M-100.5M
-	 */
-	public static function shortenHandredMillion($number)
-	{
-	    // 105,000,000
-		$length = 9;
-
-		self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 9), ($length - 6));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 5), ($length));
-        if ($others === "000000") {
-            
-            return $leading_number.'M';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 9), ($length - 5)); 
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            return (preg_match('/\.0M$/', $arr[0].$arr[1].$arr[2].'.'.$arr[3].'M')) ? $arr[0].$arr[1].$arr[2].'M' : $arr[0].$arr[1].$arr[2].'.'.$arr[3].'M';
-        }
+		return (preg_match('/\\d\.0$/', $formated_number)) ? rtrim(rtrim($formated_number,'0'), '.').$suffix : $formated_number.$suffix;
 	}
 
 	/**
 	 * Convert 1000,000,000 place to 1B
 	 * @param  int/string $number
+	 * @param int $precision Number of number after decimal point.
 	 * @return string 	A string number formated as 1B-1.5B
 	 */
-	public static function shortenBillion($number)
+	public static function formatBillion($value, $precision): string
 	{
-		// 1050,000,000
-		$length = 10;
+		$format = 1000000000;
+		// $treshold2 = 999999999; 
+		$range = [999999999, 999999999999];
+		$suffix = 'B';
+		// Check if value is a valid integer and does not start with 0, and force user to pass value as string
+		self::validateNumber($value);
+		self::validateRange($value, $range);
 
-		self::validateNumber($number);
-		self::checkLength($number, $length);
-		// Number to return shortened
-	    $leading_number = substr((string)$number, ($length - 10), ($length - 9));
-        // Number excluded/hide, also use to determine if what contains other than integer than 0.
-        $others = substr((string)$number, ($length - 9), ($length));
-        if ($others === "000000000") {
-            
-            return $leading_number.'B';
-        }
-        else {
-            // Alternate form of shortened number 1.5K
-            $alt_leading_number = substr((string)$number, ($length - 10), ($length - 8)); 
-            // Array of excluded number
-            $arr = str_split($alt_leading_number);
-            // If the remaining number as this format 1.0K transform it to 1K if the remaining number were 090 for 1090 => 1.0 as the reaming number dosent contains only 0s.
-            return (preg_match('/\.0B$/', $arr[0].'.'.$arr[1].'B')) ? $arr[0].'B' : $arr[0].'.'.$arr[1].'B';
-        }   
-	}
+		if (isset(self::$billion_format))
+			$clean_number = $value / self::$billion_format;
+		$clean_number = $value / $format;
+		// Round and format number
+		$formated_number = number_format($clean_number,$precision);
 
-	/**
-	 * Convert 10,000,000,000 place to 10B
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 10B-10.5B
-	 */
-	public static function shortenTenBillion($number)
-	{
-	    
-	}
-
-	/**
-	 * Convert 100,000,000,000 place to 100B
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 100B-100.5B
-	 */
-	public static function shortenHandredBillion($number)
-	{
-	    
+		return (preg_match('/\\d\.0$/', $formated_number)) ? rtrim(rtrim($formated_number,'0'), '.').$suffix : $formated_number.$suffix;  
 	}
 
 	/**
 	 * Convert 1,000,000,000,000 place to 1T
 	 * @param  int/string $number
+	 * @param int $precision Number of number after decimal point.
 	 * @return string 	A string number formated as 1B-1.5B
 	 */
-	public static function shortenTrillion($number)
+	public static function formatTrillion($value, $precision): string
 	{
-	    
-	}
+		$format = 1000000000000;
+		// $treshold2 = 999999999999; 
+		$range = [999999999999, 999999999999000];
+		$suffix = 'T';
+		// Check if value is a valid integer and does not start with 0, and force user to pass value as string
+		self::validateNumber($value);
+		self::validateRange($value, $range);
 
-	/**
-	 * Convert 10,000,000,000,000 place to 10B
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 10B-10.5B
-	 */
-	public static function shortenTenTrillion($number)
-	{
-	    
-	}
+		if (isset(self::$trillion_format))
+			$clean_number = $value / self::$trillion_format;
+		$clean_number = $value / $format;
+		// Round and format number
+		$formated_number = number_format($clean_number,$precision);
 
-	/**
-	 * Convert 100,000,000,000,000 place to 1B
-	 * @param  int/string $number
-	 * @return string 	A string number formated as 1B-1.5B
-	 */
-	public static function shortenHandredTrillion($number)
-	{
-	    
+		return (preg_match('/\\d\.0$/', $formated_number)) ? rtrim(rtrim($formated_number,'0'), '.').$suffix : $formated_number.$suffix; 
 	}
 
 
@@ -361,9 +181,9 @@ class ShortenNums
 	 * @param  string $number the invalid number
 	 * @return string
 	 */
-	protected static function notSupported($number)
+	private static function notSupported($number): string
 	{
-	    echo 'Sorry ' . $number . ' is not Yet Supported!';
+	    return 'Sorry ' . $number . ' is not Supported!';
 	}
 
 	/**
@@ -371,35 +191,23 @@ class ShortenNums
 	 * @param  [type] $number [description]
 	 * @return [type]         [description]
 	 */
-	protected static function validateNumber($number)
+	private static function validateNumber($number)
 	{
 		// Tis temporary enforce that the pass value should be pass as string of an integer instead non quoted integer example: '1000' is valid, 1000 is not.
-		
 		if (!is_string($number))
 			throw new \Exception("TypeError: $number should be pass as quoted string to avoid unespected result.", 1);
 		// Accept only integers and string with valid integers.
     	if (!is_int((int)$number))
     		throw new \Exception("TypeError: $number is not a valid integer", 1);
-    	// Should not start with 0.
-    	if (!preg_match('/((?!(0))^[\d]{1,10})$/i', (string)$number))
+    	// Should not start with 0. Also check the number length
+    	if (!preg_match('/((?!(0))^[\d]+)$/i', (string)$number))
     		throw new \Exception("Error: $number is not valid, value should not start with 0", 1);
-    	// length match $number lenght
+    	// Invalid number TODO:  Allow negative numbers
 	}
 
-	protected static function validateLength($length, $number, $number_length)
+	private static function validateRange($number, $range)
 	{
-	    if (isset($length) && $length != $number_length)
-	    	throw new \Exception("Error: The provided length=\"$length\" does not match the number=\"$number\" length.");
-    	// Value start from one thousand (1000) & below one billion.
-    	// Will add up to hundred trillion in future.
-	    if (isset($length) && ($length < 4 || $length > 10))
-	        throw new \Exception("Error: The provide lenght=$length is not a supported values, we can only shorten values with length 4-10 / 1K-1B");
-	}
-
-	protected static function checkLength($number, $length)
-	{
-		$number_length = strlen((string)$number);
-		if ($length != $number_length)
-			throw new \Exception("Error: The provided length=\"$length\" does not match the number=\"$number\" length.");
+		if ((int)$number < $range[0] || (int)$number > $range[1])
+			throw new \Exception("Error: The provided value \"$number\" is not in the range of \"$range[0]-$range[1]\".");
 	}
 }
