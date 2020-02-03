68d24464272ea5708446e7fa80c2a0df9553ec1b
Cedric Megnie <blackleeder@gmail.com>
2020-02-03 10:26:07 +0000

    updated test

--- tests/ShortenNumsTest.php
+++ tests/ShortenNumsTest.php
@@ -10,36 +10,36 @@ class ShortenNumsTest extends TestCase
     /** @test */
     public function it_can_shortent_any_valid_number()
     {
-    	$random = ShortenNumsFacade::readableNumber('2000',3);
-        $this->assertRegExp('/((?!(0))^([\d]{1,3})[kMBT]$|((?!(0))^[\d]{1,3}\.\d{1,}[kMBT]$))/i', $random);
+    	$random = ShortenNumsFacade::readableNumber('0',3);
+        $this->assertRegExp('/((?!(0\d))^([\d]{1,3})[kMBT]$|((?!(0\d))^[\d]{1,3}\.\d{1,}[kMBT]|((?!(0\d))^[\d]{1,3}|(?!(0\d))^[\d]{3}\+T$)))/i', $random);
     }
 
     /** @test */
     public function it_can_shorten_thousands()
     {
         $thousands = ShortenNumsFacade::readableThousand('120300');
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}k$|((?!(0))^[\d]{1,3}\.\d{1,}k$))/i', $thousands);
+        $this->assertRegExp('/((?!(0\d))^[\d]{1,3}k$|((?!(0\d))^[\d]{1,3}\.\d{1,}k$))/i', $thousands);
     }
 
     /** @test */
     public function it_can_shortent_millions()
     {
         $millions = ShortenNumsFacade::readableMillion('359050235');
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}M$|((?!(0))^[\d]{1,3}\.\d{1,}M$))/i', $millions);
+        $this->assertRegExp('/((?!(0\d))^[\d]{1,3}M$|((?!(0\d))^[\d]{1,3}\.\d{1,}M$))/i', $millions);
     }
 
     /** @test */
     public function it_can_shorten_billions()
     {
-        $billion = ShortenNumsFacade::readableBillion('4077578213');
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}B$|((?!(0))^[\d]{1,3}\.\d{1,}B$))/i', $billion);
+        $billion = ShortenNumsFacade::readableBillion(4077578213);
+        $this->assertRegExp('/((?!(0\d))^[\d]{1,3}B$|((?!(0\d))^[\d]{1,3}\.\d{1,}B$))/i', $billion);
     }
 
     /** @test */
     public function it_can_shorten_trillions()
     {
         $trillions = ShortenNumsFacade::readableTrillion(407757821309809,4);
-        $this->assertRegExp('/((?!(0))^[\d]{1,3}T$|((?!(0))^[\d]{1,3}\.\d{1,}T$))/i', $trillions);
+        $this->assertRegExp('/((?!(0\d))^[\d]{1,3}T$|((?!(0\d))^[\d]{1,3}\.\d{1,}T$))/i', $trillions);
     }
     /**
      * @test
@@ -48,6 +48,6 @@ class ShortenNumsTest extends TestCase
     {
         $invalid_value = '9999999999999999';
         $default = ShortenNumsFacade::readableNumber($invalid_value);
-        $this->assertRegExp('/((?!(0))^[\d]{3}\+T$|((?!(0))^[\d]{1,3}$))/i', $default);
+        $this->assertRegExp('/((?!(0\d))^[\d]{3}\+T$|((?!(0\d))^[\d]{1,3}$))/i', $default);
     }
 }
